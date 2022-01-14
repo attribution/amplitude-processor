f7464498fe0df97d74c43aff3cd455a5d9b98023
Maxim Sadovsky <maksim@sadovskij.com>
2022-01-14 14:18:15 +0200

    Fix counting processed records

--- lib/amplitude-processor/processors/null.rb
+++ lib/amplitude-processor/processors/null.rb
@@ -1,11 +1,17 @@
 module AmplitudeProcessor
   module Processors
     class Null
-      def track(attrs); end
+      def track(attrs)
+        true
+      end
 
-      def identify(attrs); end
+      def identify(attrs)
+        true
+      end
 
-      def page(attrs); end
+      def page(attrs)
+        true
+      end
     end
   end
 end
